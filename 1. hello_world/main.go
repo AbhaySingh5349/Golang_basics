e582600690d6964e2506dcf4847e7e940337c70d
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-01-28 14:19:49 +0530

    basic hello world program

---
+++ 1. hello_world/main.go
@@ -0,0 +1,10 @@
+// package = project = worksapce (collection of common src code files / to group together code with similar purpose)
+
+// used for creating executable type package to spit out runnable file
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Print("hello world golang")
+}
